0d3021815bdea3e164022290c853a4e0dfb871cf
Krish Sethi <95207265+Krish-198@users.noreply.github.com>
2025-01-14 19:06:56 +0530

    Add files via upload

--- mergesort.py
+++ mergesort.py
@@ -1,6 +1,6 @@
 #Divide and Conquer Algorithm
 #Time Complexity - O(nlogn)
-def merge(l,low,mid,high,a):
+def merge(a,low,mid,high):
     l=[]
     start1=low
     start2=mid+1
@@ -17,4 +17,18 @@ def merge(l,low,mid,high,a):
     while start2<=high:
         l.append(a[start2])
         start2=start2+1
+    p=0
+    for i in range(low,high+1):
+        a[i]=l[p]
+        p+=1
+def mergesort(x,low,high):
+    if low<high:
+        mid=(low+high)//2
+        mergesort(x,low,mid) #First Partition
+        mergesort(x,mid+1,high) #Second Partition
+        merge(x,low,mid,high)
+    
 a=[12,54,78,43,9,72,0,45,23]
+j=len(a)
+mergesort(a,0,j-1)
+print(a)
